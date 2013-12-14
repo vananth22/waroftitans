2b021893125a6eb823ffb2f75384cb19c36ba05d
Ananth Durai <vananth22@gmail.com>
2013-12-14 19:58:52 +0530

    Entity classifier to static inner variable 	modified:   src/main/java/org/ananth/waroftitans/analyser/EntityClassifier.java

--- src/main/java/org/ananth/waroftitans/analyser/EntityClassifier.java
+++ src/main/java/org/ananth/waroftitans/analyser/EntityClassifier.java
@@ -8,29 +8,35 @@ import edu.stanford.nlp.ling.CoreLabel;
 
 /**
  * @author Ananth
- *
  */
 public class EntityClassifier {
-    
+
     /**
      * Make it Singleton
      */
-    
-    private EntityClassifier() {};
-    
+
+    private EntityClassifier() {
+    };
+
     private static class SingletonHelper {
-        private static final EntityClassifier entity = new EntityClassifier();
+        private static final EntityClassifier   entity     = new EntityClassifier();
+        private static CRFClassifier<CoreLabel> classifier = null;
+
+        static {
+            if (classifier == null) {
+                final String serializedClassifier = "edu/stanford/nlp/models/ner/english.all.3class.distsim.crf.ser.gz";
+                classifier = CRFClassifier.getClassifierNoExceptions(serializedClassifier);
+            }
+        }
     }
-    
-    public static EntityClassifier getInstance() {
+
+    public static EntityClassifier getInstance () {
         return SingletonHelper.entity;
     }
-    
-    
-    public CRFClassifier<CoreLabel> getCRFClassifier() {        
-         final String serializedClassifier = "edu/stanford/nlp/models/ner/english.all.3class.distsim.crf.ser.gz";
-         return CRFClassifier.getClassifierNoExceptions(serializedClassifier);
-        
+
+    public CRFClassifier<CoreLabel> getCRFClassifier () {
+        return SingletonHelper.classifier;
+
     }
 
 }
